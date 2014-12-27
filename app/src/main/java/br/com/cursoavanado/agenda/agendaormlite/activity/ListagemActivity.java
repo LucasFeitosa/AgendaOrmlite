ddf6647c847bff63ddce295ddcafda6a55d9600d
Lucas Feitosa <luucasfeitosa@hotmail.com>
2014-12-27 12:09:36 -0400

    Criação da funcionalidade de cadastro de Contatos//2

--- app/src/main/java/br/com/cursoavanado/agenda/agendaormlite/activity/ListagemActivity.java
+++ app/src/main/java/br/com/cursoavanado/agenda/agendaormlite/activity/ListagemActivity.java
@@ -84,7 +84,7 @@ public class ListagemActivity extends ActionBarActivity {
         }
         dao.close();
     }
-
+    //teste
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
